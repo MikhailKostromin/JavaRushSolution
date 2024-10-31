2d1e23e7c8b59f280ffb63aa2d0330e5df4feda4
mihailkostromin <kostrominmi@mail.ru>
2024-10-31 17:53:28 +0300

    Solution from Stream to String

--- src/Solutions/FromStreamToString.java
+++ src/Solutions/FromStreamToString.java
@@ -1,4 +1,24 @@
 package Solutions;
 
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
+
 public class FromStreamToString {
+
+/*
+Из потока данных — в строку
+*/
+
+    public static void main(String[] args) {
+        var stringStream = Stream.of("To", "be", "a", "programmer", "you", "need", "to", "code");
+
+        System.out.println(getString(stringStream));
+    }
+
+    public static String getString(Stream<String> stringStream) {
+        //напишите тут ваш код
+        return stringStream.collect(joining(" "));
+    }
 }
+
