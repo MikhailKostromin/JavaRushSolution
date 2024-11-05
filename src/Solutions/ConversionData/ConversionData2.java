2bb6ef37422c191b4c52605ea3c3d766570be568
mihailkostromin <kostrominmi@mail.ru>
2024-11-05 17:34:16 +0300

    Add solution  ConversationData2

--- src/Solutions/ConversionData/ConversionData2.java
+++ src/Solutions/ConversionData/ConversionData2.java
@@ -1,4 +1,21 @@
 package Solutions.ConversionData;
 
+import java.util.stream.Stream;
+
+/*
+Преобразование данных-2
+*/
+
 public class ConversionData2 {
+
+    public static void main(String[] args) {
+        Stream<String> stream = Stream.of("Не", "волнуйтесь", "если", "что-то", "не", "работает.", "Если", "бы", "всё", "работало", "вас", "бы", "уволили.");
+
+        toUpperCase(stream).forEach(System.out::println);
+    }
+
+    public static Stream<String> toUpperCase(Stream<String> strings) {
+        //напишите тут ваш код
+        return strings.map(String::toUpperCase);
+    }
 }
